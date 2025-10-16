5ae08e412f1601111fc4de59fbd9cbb5e030fa22
Vasek-Doskar <doskar@spsjc.cz>
2025-10-16 13:02:58 +0200

    Přidejte soubory projektu.

---
+++ Clovek.cs
@@ -0,0 +1,83 @@
+﻿namespace ZakladyOOP_I2B1
+{
+    public class Clovek
+    {
+        //fields (Atributy)
+        private string _jmeno;
+        private int _vek;
+        private string _prijmeni;
+
+        // properties (Vlastnosti)
+        public string Jmeno
+        {
+            get
+            {
+                return _jmeno;
+            }
+            set
+            {
+                if (_jmeno != value && value.Length >= 3)
+                {
+                    _jmeno = value;
+                }
+            }
+        }
+
+        public string Prijmeni
+        {
+            get
+            {
+                return _prijmeni;
+            }
+            set
+            {
+                if (_prijmeni != value && value.Length >= 3)
+                {
+                    _prijmeni = value;
+                }
+            }
+        }
+
+
+        public int Vek
+        {
+            get
+            {
+                return _vek;
+            }
+            set
+            {
+                value = Math.Abs(value);
+
+                if (value > 110)
+                    throw new Exception($"Tento věk není ({value}) možný!");
+                else
+                    _vek = value;
+            }
+        }
+
+        public Clovek(string jmeno, string prijmeni, int vek)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            Vek = vek;
+        }
+        #region MyRegion
+
+        public string Pozdrav() =>
+            $"Jsem {_jmeno} {Prijmeni}, a je mi {_vek}let";
+
+        public string Pozdrav(string pozdrav)
+        {
+            return $"{pozdrav}! " + Pozdrav();
+        }
+
+        public string Pozdrav(string pozdrav, string koho)
+        {
+            return $"{pozdrav} {koho}!" + Pozdrav();
+        }
+        #endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ZakladyOOP_I2B1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Clovek Jonas = new Clovek("Jonáš", "Macoun", 5);
+            Jonas.Jmeno = "Jonda"; // set
+            Jonas.Vek = 155;
+            Console.WriteLine(Jonas.Jmeno); // get
+
+            Jonas.Pozdrav();
+        }
+    }
+}
